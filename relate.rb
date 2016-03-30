922b2fe329eead2c044746579368e4d4eb291937
rheber <rheber@rediffmail.com>
2016-03-30 14:38:06 +1100

    open csv files

--- relate.rb
+++ relate.rb
@@ -1,1 +1,11 @@
-print "Hello, world!"
+require "CSV"
+
+def main
+  CSV.foreach("relations/"+ARGV[0]) do |row|
+    print row
+  end
+rescue Errno::ENOENT
+  STDERR.print "relate: Invalid file '#{ARGV[0]}'"
+end
+
+main
