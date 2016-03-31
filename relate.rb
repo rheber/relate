4aa6b188db6ab047e229ed1dd957fad1587442fe
rheber <rheber@rediffmail.com>
2016-03-31 18:34:57 +1100

    make hash

--- relate.rb
+++ relate.rb
@@ -1,11 +1,23 @@
 require "CSV"
 
 def main
-  CSV.foreach("relations/"+ARGV[0]) do |row|
-    print row
+  csv = CSV.read("relations/"+ARGV[0]+".csv")
+  header = csv[0].map{|h| h.downcase}
+  key = header.index(ARGV[1].downcase)
+  val = header.index(ARGV[2].downcase)
+
+  questions = Hash.new
+  begin
+    csv[1..-1].each { |row| questions[row[key]] = row[val] }
+  rescue TypeError
+    STDERR.print "relate: Invalid heading"
   end
 rescue Errno::ENOENT
   STDERR.print "relate: Invalid file '#{ARGV[0]}'"
+rescue NoMethodError
+  STDERR.print "relate: Too few arguments" # Called downcase on nil.
+rescue TypeError
+  STDERR.print "relate: Too few arguments" # Called + on nil.
 end
 
 main
